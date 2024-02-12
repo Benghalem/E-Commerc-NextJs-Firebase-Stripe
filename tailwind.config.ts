89eb58979883d757609725b42a4df7b0e01b5618
benghalem <benghalemaziz@gmail.com>
2024-02-12 20:23:34 +0100

    Creating Product and Footer Component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,34 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    maxWidth: {
+      container: "1520px",
+      contenetContainer: "1280px",
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
+      },
+      screens: {
+        xs: "320px",
+        sm : "375px",
+        sml: "500px",
+        md: "667px",
+        mdl: "780px",
+        lg: "960px",
+        lgl: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+      colors: {
+        blue: "#0071dc",
+        lighBlue: "#e6f1fc",
+        yellow: "#ffc220",
+        hoverBg: "#004f9a",
+        lightText: "#46474a",
+      },
+      boxShadow: {
+        bannerShadow: "0px 1px 2px 1px #00000026",
       },
     },
   },
